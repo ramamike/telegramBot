dec91db6d52a2372ba131178f2b2cc9a4a6edc7e
mihal <7629756@gmail.com>
2023-05-06 23:21:05 +0300

    feat: add druft menu

--- src/main/java/com/springLearnig/telegramBot/service/TelegramBotService.java
+++ src/main/java/com/springLearnig/telegramBot/service/TelegramBotService.java
@@ -4,9 +4,15 @@ import com.springLearnig.telegramBot.config.BotConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 @Slf4j
@@ -17,6 +23,17 @@ public class TelegramBotService extends TelegramLongPollingBot {
 
     public TelegramBotService(BotConfig botConfig) {
         this.botConfig = botConfig;
+        List<BotCommand> commandList = new ArrayList<>();
+        commandList.add(new BotCommand("/start", "get a welcome message"));
+        commandList.add(new BotCommand("/mydata", "get your data stored"));
+        commandList.add(new BotCommand("/deletedata", "delete your data stored"));
+        commandList.add(new BotCommand("/help", "how to use"));
+        commandList.add(new BotCommand("/settings", "set your preferences"));
+        try {
+            this.execute(new SetMyCommands(commandList, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
+            log.error("Execute menu creation error :", e);
+        }
     }
 
     @Override
